787cf5a048d18cb60d2189fe9bf61407f8257aa6
GaborMartin <gabormartin94@gmail.com>
2018-01-22 11:57:50 +0100

    Implemented basic function of class Log

---
+++ src/com/codecool/Logger.java
@@ -0,0 +1,15 @@
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Logger {
+
+    public static void log(String type, String message) {
+        if (type == "Timestamp") {
+            Date date = new Date();
+            Timestamp timestamp = new Timestamp(date.getTime());
+            System.out.println("[" + timestamp + "]" + " " +message);
+        }
+
+    }
+}
