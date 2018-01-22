946319c99347c2d5b1eec87a0b44bd27310674ff
peti120 <peti59550@gmail.com>
2018-01-22 11:58:47 +0100

    Simulator base implemented

---
+++ src/com/codecool/Simulator.java
@@ -0,0 +1,13 @@
+package com.codecool;
+
+class Simulator{
+    Simulation simulation;
+    Logger logger;
+    public Simulator(Simulation simulation, Logger logger){
+        this.simulation = simulation;
+        this.logger = logger;
+    }
+    public Result run(){
+        return a; // Result type 
+    }
+}
