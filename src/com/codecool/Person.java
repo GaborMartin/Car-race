755f91c4389cc5732d78ef21b20ae3ad64fbd480
GaborMartin <gabormartin94@gmail.com>
2018-01-22 14:37:26 +0100

    Implemented generateRandomDriver and addToDriverArray methods

--- src/com/codecool/Person.java
+++ src/com/codecool/Person.java
@@ -1,25 +1,49 @@
 package com.codecool;
+import java.util.Random;
 
-class Person{
+class Person {
     private String name;
     private int drivingExeperience;
+    Person[] drivers;
 
-    public Person(String name,int drivingExeperience){
+    public Person(String name,int drivingExeperience) {
         this.name = name;
         this.drivingExeperience = drivingExeperience;
+        drivers = new Person[0];
     }
 
-    public String getName(){
+    public String getName() {
         return name;
     }
-    public void setName(String name){
+    public void setName(String name) {
         this.name = name;
     }
 
-    public int getDrivingExperience(){
+    public int getDrivingExperience() {
         return drivingExeperience;
     }
-    public void setDrivingExperience(int drivingExeperience){
+
+    public void setDrivingExperience(int drivingExeperience) {
         this.drivingExeperience = drivingExeperience;
+    }
+
+    public Person[] generateRandomDriver(int numOfCars) {
+        String[] names = {"Joe", "Jesus", "Robert", "Matt", "Peter", "Aaron", "Paul", "Astrix", "Tristan", "Ekahal"};
+        Random random = new Random();
+
+        while (drivers.length < numOfCars) {
+            Person driver = new Person(names[random.nextInt(names.length-1)], random.nextInt(10) + 1);
+            addToDriverArray(driver, drivers);
+            
+        }
+    }
+
+    public void addToDriverArray(Person driver, Person[] driverArray) {
+        Person[] tempArray = new Person[driverArray.length + 1];
+        for (int i = 0; i < driverArray.length; i++) {
+            tempArray[i] = driverArray[i];
+        }
+        tempArray[tempArray.length - 1] = driver;
+        driverArray = tempArray;
     }
 }
