05012110a500eddd38a8f12fbd3665e477fe26f7
GaborMartin <gabormartin94@gmail.com>
2018-01-23 11:13:29 +0100

    Added method to shuffle drivers and generate random car for a driver

--- src/com/codecool/Main.java
+++ src/com/codecool/Main.java
@@ -7,7 +7,8 @@ class Main{
     private static final String ANSI_RESET = "\u001B[0m";
     private static final String ANSI_GREEN = "\u001B[32m";
     public static void main(String[] args) {
-        Car[] cars = Car.generateRandomCars(50);
+        Car[] cars = Car.generateRandomCars(6);
+        Person[] drivers = Person.generateRandomDriver(6);
         for(int i = 0; i < cars.length; i++){
             boolean tuned = cars[i].getTuning();
             System.out.println(cars[i].getBrand() + " " + cars[i].getHorsepower() + " " + (tuned ? ANSI_GREEN+"Tuned"+ANSI_RESET:"Not tuned" )+ " " + cars[i].getFailurePercent() + " " + cars[i].manufacturingDate()+ " "+ANSI_RED+ cars[i].getPerformance()+ANSI_RESET);
--- src/com/codecool/Person.java
+++ src/com/codecool/Person.java
@@ -1,15 +1,18 @@
 package com.codecool;
-import java.util.Random;
+import java.util.*;
+import java.util.concurrent.ThreadLocalRandom;
 
 class Person {
     private String name;
     private int drivingExeperience;
-    Person[] drivers;
+    private static Person[] drivers = new Person[0];
+    private Car car;
+    
 
     public Person(String name,int drivingExeperience) {
         this.name = name;
         this.drivingExeperience = drivingExeperience;
-        drivers = new Person[0];
+
     }
 
     public String getName() {
@@ -27,23 +30,35 @@ class Person {
         this.drivingExeperience = drivingExeperience;
     }
 
-    public Person[] generateRandomDriver(int numOfCars) {
+    public static Person[] generateRandomDriver(int numOfCars) {
         String[] names = {"Joe", "Jesus", "Robert", "Matt", "Peter", "Aaron", "Paul", "Astrix", "Tristan", "Ekahal"};
         Random random = new Random();
 
         while (drivers.length < numOfCars) {
             Person driver = new Person(names[random.nextInt(names.length-1)], random.nextInt(10) + 1);
+            driver.car = Car.generateRandomCars(1)[random.nextInt(numOfCars) + 1];
             addToDriverArray(driver, drivers);
         }
+        shuffleDriverArray(drivers);
         return drivers;
     }
 
-    public void addToDriverArray(Person driver, Person[] driverArray) {
+    public static void addToDriverArray(Person driver, Person[] driverArray) {
         Person[] tempArray = new Person[driverArray.length + 1];
         for (int i = 0; i < driverArray.length; i++) {
             tempArray[i] = driverArray[i];
         }
         tempArray[tempArray.length - 1] = driver;
         driverArray = tempArray;
+    }
+
+    public static void shuffleDriverArray(Person[] drivers) {
+        Random rnd = ThreadLocalRandom.current();
+        for (int i = drivers.length - 1; i > 0; i--) {
+            int index = rnd.nextInt(i + 1);
+            Person p = drivers[index];
+            drivers[index] = drivers[i];
+            drivers[i] = p;
+        }
     }
 }
